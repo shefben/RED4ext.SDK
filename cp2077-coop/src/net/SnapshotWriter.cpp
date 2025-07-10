4d1a5a56710be2dfa875f1b9914b85952287b7c6
shefben <shefben@gmail.com>
2025-07-09 21:28:09 -0500

    Guard g_entitySnaps with mutex

--- cp2077-coop/src/net/SnapshotWriter.cpp
+++ cp2077-coop/src/net/SnapshotWriter.cpp
@@ -2,6 +2,7 @@
 #include "../runtime/SpectatorCam.reds"
 #include "Snapshot.hpp"
 #include <vector>
+#include <mutex>
 
 namespace CoopNet
 {
@@ -13,11 +14,25 @@ struct EntitySnap
 };
 
 std::vector<EntitySnap> g_entitySnaps;
+std::mutex g_entitySnapsMutex;
+
+void AddEntitySnap(uint32_t id, uint32_t phaseId, const TransformSnap& snap)
+{
+    std::lock_guard<std::mutex> lock(g_entitySnapsMutex);
+    g_entitySnaps.push_back({id, phaseId, snap});
+}
+
+void ClearEntitySnaps()
+{
+    std::lock_guard<std::mutex> lock(g_entitySnapsMutex);
+    g_entitySnaps.clear();
+}
 
 void BuildSnapshot(std::vector<EntitySnap>& out)
 {
     uint32_t local = QuestSync::localPhase;
     uint32_t spectate = SpectatorCam::spectatePhase;
+    std::lock_guard<std::mutex> lock(g_entitySnapsMutex);
     for (auto& e : g_entitySnaps)
     {
         if (e.phaseId != local && e.phaseId != spectate)
